0180bd44b9c034a1ba9860a80ce3566ee6b8709f
AbdallahAshraf1973 <70912306+AbdallahAshraf1973@users.noreply.github.com>
2023-01-02 21:53:16 +0200

    My First Commit

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundExceotion;
+use Illuminate\Support\Facades\File;
+class Post{
+    public static function all(){
+        $files= File::files(resource_path("/../resources/posts/"));
+        return array_map(fn($file) =>$file ->getContents(), $files);
+    }
+
+    public static function find($slug){
+        if(! file_exists($path= resource_path("/../resources/posts/{$slug}.html")))
+    {
+        throw ModelNotFoundExceotion();
+    }
+    return $post = cache()->remember("posts.{$slug}",now()->addMinutes(20), fn() => file_get_contents($path));
+    
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $posts=Post::all();
+    return view('posts',[
+        'posts' => $posts
+    ]);
+});
+Route::get('/posts/{post}', function($slug) {
+    //Find post by ot slug and pass it to view called post
+    $post= Post::find($slug);
+    return view('post',[
+    'post'=>  $post
+    ]);
+}) ->where('post','[A-z_\-]+');
