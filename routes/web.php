b3cc99210c6589dbfbb877b35e46c5bbb535fff8
AbdallahAshraf1973 <70912306+AbdallahAshraf1973@users.noreply.github.com>
2023-01-22 07:36:19 +0200

    Show-All-Posts-Associated-With-a-Category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\Category;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +18,14 @@ Route::get('/', function () {
         'posts' => Post::all()
     ]);
 });
-Route::get('/posts/{post:slug}', function(Post $post) {
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post',[
     'post'=>  $post
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts',[
+        'posts' => $category->posts
     ]);
 });
