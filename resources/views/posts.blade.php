c3306332093882178fde8e261abf21b71989df29
AbdallahAshraf1973 <70912306+AbdallahAshraf1973@users.noreply.github.com>
2023-01-25 08:13:55 +0200

    Convert-the-HTML-and-CSS

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,16 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
+            @include ('_posts-header');
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
 
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href ="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-            </p> 
+            @if($posts->count())
+            
+            <x-posts-grid :posts="$posts" />
+            @else
+            <p class="text-center">No Posts yet. please check back later.</p>
+            @endif
+        </main>
 
-            
-            
-            
-            <div>
-                {!!$post->excerpt!!}
-            </div>   
-        </article>
-    @endforeach    
+
 </x-layout>    
