079b441b5cdb2db9ea4f61d0d0aa8057c105545d
AbdallahAshraf1973 <70912306+AbdallahAshraf1973@users.noreply.github.com>
2023-02-01 05:10:46 +0200

    Author Filtering

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,3 @@ use App\Models\Category;
 Route::get('/',[PostController::class ,'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class ,'show']);
-
-/* oute::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts',[
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-}); */
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts',[
-        'posts' => $author->posts
-    ]);
-});
