1d3d457ebf2d317fcf3e003e33b9c6f35c1745e7
AbdallahAshraf1973 <70912306+AbdallahAshraf1973@users.noreply.github.com>
2023-02-04 12:04:31 +0200

    Login and Logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +19,6 @@ use App\Http\Controllers\RegisterController;
 Route::get('/',[PostController::class ,'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class ,'show']);
-Route::get('register',[RegisterController::class,'create']);
-Route::post('register',[RegisterController::class,'store']);
+Route::get('register',[RegisterController::class,'create'])->middleware('guest');
+Route::post('register',[RegisterController::class,'store'])->middleware('guest');
+Route::post('logout',[SessionsController::class,'destroy']);
